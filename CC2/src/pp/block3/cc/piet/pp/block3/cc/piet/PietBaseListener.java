492eba0be11052a9ab69a61d8aed048dbbc85e13
QWolf <peter.schroten@hotmail.com>
2015-05-13 10:29:16 +0200

    Scopespul

---
+++ CC2/src/pp/block3/cc/piet/pp/block3/cc/piet/PietBaseListener.java
@@ -0,0 +1,125 @@
+// Generated from Piet.g4 by ANTLR 4.5
+package pp.block3.cc.piet;
+package pp.block3.cc.piet;
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+/**
+ * This class provides an empty implementation of {@link PietListener},
+ * which can be extended to create a listener which only needs to handle a subset
+ * of the available methods.
+ */
+public class PietBaseListener implements PietListener {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterEqual(PietParser.EqualContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitEqual(PietParser.EqualContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterNumber(PietParser.NumberContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitNumber(PietParser.NumberContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterBoolean(PietParser.BooleanContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitBoolean(PietParser.BooleanContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterString(PietParser.StringContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitString(PietParser.StringContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterBracket(PietParser.BracketContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitBracket(PietParser.BracketContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterHat(PietParser.HatContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitHat(PietParser.HatContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterPlus(PietParser.PlusContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitPlus(PietParser.PlusContext ctx) { }
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitTerminal(TerminalNode node) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitErrorNode(ErrorNode node) { }
+}
--- CC2/src/pp/block3/cc/symbol/MySymbolTable.java
+++ CC2/src/pp/block3/cc/symbol/MySymbolTable.java
@@ -1,33 +1,55 @@
 package pp.block3.cc.symbol;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MySymbolTable implements SymbolTable {
 	
-	HashMap blub = new Hashmap();
+	public MySymbolTable(){
+		scopes.put(0, new ArrayList<String>());
+	}
+	
+	Map<Integer, List<String>>  scopes = new HashMap<Integer, List<String>>();
+	int scopelevel = 0;
 
 	@Override
 	public void openScope() {
-		// TODO Auto-generated method stub
+		scopelevel++;
+		scopes.put(scopelevel, new ArrayList<String>());
 		
 	}
 
 	@Override
 	public void closeScope() {
-		// TODO Auto-generated method stub
+		if(scopelevel == 0){
+			throw new RuntimeException("Can't close top-level scope");
+		}
+		scopes.remove(scopelevel);
+		scopelevel--;
+		
+		
 		
 	}
 
 	@Override
 	public boolean add(String id) {
-		// TODO Auto-generated method stub
-		return false;
+		if (scopes.get(scopelevel).contains(id))
+			return false;
+		else {
+			scopes.get(scopelevel).add(id);
+			return true;
+		}
 	}
 
 	@Override
 	public boolean contains(String id) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean result = false;
+		for (int i = scopelevel; i >= 0; i--){
+			result |= scopes.get(i).contains(id);
+		}		
+		return result;
 	}
 
 }
