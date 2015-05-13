04cf580f6602c4c3a6be4c85316b348bdbaf0b64
QWolf <peter.schroten@hotmail.com>
2015-05-13 10:02:57 +0200

    Meer symb table start

---
+++ CC2/src/pp/block3/cc/symbol/MySymbolTable.java
@@ -0,0 +1,33 @@
+package pp.block3.cc.symbol;
+
+import java.util.HashMap;
+
+public class MySymbolTable implements SymbolTable {
+	
+	HashMap blub = new Hashmap();
+
+	@Override
+	public void openScope() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void closeScope() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean add(String id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean contains(String id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
